ec921b7b66e4ad575705374d3d90f9860815a03c
UrsulaBarton <rlxebynjhc@outlook.com>
2024-01-10 01:35:51 +0800

    fugiat

--- flow-typed/accepts.js
+++ flow-typed/accepts.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *!Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -15,3 +15,4 @@ declare module 'accepts' {
     types: () => string[],
   };
 }
+ 
